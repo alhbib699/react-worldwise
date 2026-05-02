9efa9d50526937f258c76878f334288ccb62b473
alhbib699 <alhbib699@gmail.com>
2026-05-02 19:58:40 +0300

    implement data fetching and display cities list in sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,49 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import"./index.css"
+import "./index.css";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import HomePage from "./pages/HomePage";
 import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
+import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:9000";
+
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("there is a problem with loading data");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/app" element={<AppLayout/>}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<CityList cities={cities} isLoading={isLoading}/>} />
+          <Route path="cities" element={<CityList cities={cities} isLoading={isLoading}/>} />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,11 @@
 import { Link } from "react-router-dom";
 import styles from "./Homepage.module.css";
-import PageNav from "../components/PageNav"
-
+import PageNav from "../components/PageNav";
 
 export default function Homepage() {
   return (
     <main className={styles.homepage}>
-      <PageNav/>
+      <PageNav />
       <section>
         <h1>
           You travel the world.
@@ -18,7 +17,6 @@ export default function Homepage() {
           of. Never forget your wonderful experiences, and show your friends how
           you have wandered the world.
         </h2>
-        {console.log(document.querySelector('.cta'))}
         <Link to="/app" className="cta">
           Start tracking now
         </Link>
