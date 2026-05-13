7cc8fcb372fd8bd0359728d3724112fccb8b0b98
alhbib699 <alhbib699@gmail.com>
2026-05-13 17:33:15 +0300

    feat: add new city functionality

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -2,9 +2,11 @@ import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css";
 import { useCities } from "../contexts/CitiesContext";
 const flagEmojiToPNG = (flag) => {
+  if (!flag || typeof flag !== "string") return null;
   var countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
     .map((char) => String.fromCharCode(char - 127397).toLowerCase())
     .join("");
+  if (countryCode.length !== 2) return null;
   return (
     <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt="flag" />
   );
@@ -18,15 +20,28 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function CityItem({ city }) {
-  const {currentCity} = useCities();
+  const { currentCity } = useCities();
   const { cityName, emoji, date, id, position } = city;
+
   return (
-    <li >
-      <Link className={`${styles.cityItem} ${currentCity.id === id ? styles["cityItem--active"] : ""}`} to={`${id}?lat=${position.lat}&lng=${position.lng}`}>
-      <span className={styles.emoji}>{flagEmojiToPNG(emoji)}</span>
-      <h3 className={styles.name}>{cityName}</h3>
-      <time className={styles.date}>{formatDate(date)}</time>
-      <button className={styles.deleteBtn}>&times;</button>
+    <li>
+      <Link
+        className={`${styles.cityItem} ${currentCity.id === id ? styles["cityItem--active"] : ""}`}
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+      >
+         <span className={styles.emoji}>
+          {emoji && emoji.length === 2 ? (
+            <img 
+              src={`https://flagcdn.com/24x18/${emoji.toLowerCase()}.png`} 
+              alt="flag" 
+            />
+          ) : (
+            flagEmojiToPNG(emoji)
+          )}
+        </span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.date}>({formatDate(date)})</time>
+        <button className={styles.deleteBtn}>&times;</button>
       </Link>
     </li>
   );
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,11 +6,17 @@ import BackButton from "./BackButton";
 import { useUrlPosition } from "../hooks/useUrlPosition";
 import Message from "./Message";
 import Spinner from "./Spinner";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useCities } from "../contexts/CitiesContext";
+import { useNavigate } from "react-router-dom";
 
 const BASE_URL = "https://api.bigdatacloud.net/data/reverse-geocode-client";
 
 function Form() {
   const [lat, lng] = useUrlPosition();
+  const {createCity ,isLoading} = useCities();
+  const navigate = useNavigate();
   const [isLoadingGeocoding, setIsLoadingGeocoding] = useState(false);
   const [cityName, setCityName] = useState("");
   const [country, setCountry] = useState("");
@@ -21,6 +27,7 @@ function Form() {
 
   useEffect(
     function () {
+      if (!lat && !lng) return;
       async function FetchCityData() {
         try {
           setIsLoadingGeocoding(true);
@@ -47,11 +54,28 @@ function Form() {
     [lat, lng],
   );
 
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if(!cityName || !date) return;
+    const newCity={
+      cityName,
+      country,
+      emoji,
+      date,
+      notes,
+      position:{lat,lng} ,
+    }
+    await createCity(newCity);
+    navigate('/app/cities')
+  }
+
   if (isLoadingGeocoding) return <Spinner />;
+  if (!lat && !lng)
+    return <Message message="Start by clicking somewhere on the map" />;
   if (geocodingError) return <Message message={geocodingError} />;
 
   return (
-    <form className={styles.form}>
+    <form className={`${styles.form} ${isLoading ? styles.loading : ""}`} onSubmit={handleSubmit}>
       <div className={styles.row}>
         <label htmlFor="cityName">City name</label>
         <input
@@ -68,10 +92,12 @@ function Form() {
 
       <div className={styles.row}>
         <label htmlFor="date">When did you go to {cityName}?</label>
-        <input
+
+        <DatePicker
           id="date"
-          onChange={(e) => setDate(e.target.value)}
-          value={date}
+          onChange={(date) => setDate(date)}
+          selected={date}
+          dateFormat="dd/MM/yyyy"
         />
       </div>
 
